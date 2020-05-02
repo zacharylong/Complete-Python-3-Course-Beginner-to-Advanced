2169eb0cfb6a7f9bc23cb2156b28b01e3ea37b35
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-05-02 02:32:40 -0400

    Add files via upload
    Simple web scraper with BeautifulSoup parsing the page

---
+++ Module7/main.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+
+search = input("Enter search term: ")
+params = {"q": search}
+r = requests.get("http://www.bing.com/search", params=params)
+
+soup = BeautifulSoup(r.text, "html.parser")
+results = soup.find("ol", {"id": "b_results"})
+links = results.findAll("li", {"class": "b_algo"})
+
+for item in links:
+    item_text = item.find("a").text
+    item_href = item.find("a").attrs["href"]
+
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
+        print("Parent: ", item.find("a").parent.parent.find("p").text)
+
+        children = item.find("h2")
+        print("Next sibling of the h2: ", children.next_sibling)
